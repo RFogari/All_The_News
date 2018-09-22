56fa86e5e4e33c0822047f16b15a166ec2ffc254
rfogari <fogari10@gmail.com>
2018-09-22 19:27:35 -0400

    Updates to server.js added Models Public folders.

--- server.js
+++ server.js
@@ -1,4 +1,32 @@
+//npm required packages
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var hbs = require('express-handlebars');
+var cheerio = require('cheerio');
 
+//require models
+var db = require('./models');
+
+//port setting
+var PORT = process.env.PORT || 3000;
+
+//initialize Express
+var app = express();
+
+//Use body-parser to hadle form submission
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//public folder as stactic directory for front-end HTML/CSS.
+app.use(express.static('public'));
+
+
+//Handlebars setup
+app.engine('handlebars', hbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+
+
+//Connect to the Mongo DB
+mongoose.connect("")
