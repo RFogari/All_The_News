1efb66eec0adb96432182d314ecc81ff6bd2aa15
rfogari <fogari10@gmail.com>
2018-09-24 01:10:51 -0400

    Updated update route on newscontroller.js file.

--- controller/newscontroller.js
+++ controller/newscontroller.js
@@ -14,7 +14,7 @@ var article = require('../models/article.js');
 
 
 //Get route for scraping artciles from NYTimes website.
-app.get('/scrape', fucntion(req, res) {
+app.get('/scrape', fucntion(req, res){
 
     //Making a request for articles from the New York Times.
 
@@ -25,37 +25,75 @@ app.get('/scrape', fucntion(req, res) {
         
         //Each artilce has a headline class
         $('css-6p61n1').each(function(i, element) {
-            var headline = $(element).children('balancedHeadline').text();
-            var summary = $(element).children('css-balf30 e1n8kpyg0').text();
-            var link = $(element).children("a").attr("href");
+
+            ///result object
+            var result = {};
+
+            result.headline = $(this).children('balancedHeadline').text();
+            result.summary = $(this).children('css-balf30 e1n8kpyg0').text();
+            result.link = $(this).children("a").attr("href");
 
             //if function to validate element has all 3 requirements
 
             if(headline && summary && link) {
 
                 //insert scraped data into Mongo DB
-                db.newsData.insert({
-                    headline: headline,
-                    summary: summary,
-                    link: link,
-                },
+                db.Article.create(result)
+                .then(function(dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function(err) {
+                    return res.json(err);
+                });
+            };
+                
+        });   
 
-                    function(err, inserted) {
-                        
-                        //log error if issue arises while adding record to DB.
-                        if (err) {
-                            console.log(err);
-                        } else {
-                            console.log(inserted);
-                        };
-                    }
-            
-                )
-            }    
-        });        
-
-
-
+        res.send('Articles Have Been Scraped');
     });
-
 });
+
+//Route to retrieve all data from the DB
+app.get('/articles', function (req, res) {
+    //query to find all scraped data in DB
+    db.Article.find({})
+
+        //found articles are sent back to the browser
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        })
+
+        //if error, catch error.
+        .catch(function(err) {
+            res.json(err);
+        });
+});
+
+
+//Route to grab article by id - this will be for leaving notes
+app.get('/articles/:id', function(req, res) {
+    //query to finnd the article in the DB by searching on the ID
+    db.article.findOne({ __id: req.paramus.id })
+
+    //After query for article, populate associated notes
+    .populate('note')
+    .then(function(dbArticle) {
+        //if article found, send back to the frontend
+        res.json(dbArticle);
+    })
+    .catch(function(err) {
+        res.json(err
+    }
+})
+
+
+//Route to save articles and updating notes on articles.
+app.post('/articles/:id', function(req, res) {
+    //creating a new note.  Req.body is from the client side.
+    db.Note.create(req.body)
+        .then(function(dbNote) {
+            //after note is created, note is added to article based on article ID
+
+
+        })
+})
