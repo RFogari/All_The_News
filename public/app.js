0feab28f9a3e64f4f83d0264a18ffc99a357f474
rfogari <fogari10@gmail.com>
2018-10-14 18:00:27 -0400

    updates to app.js and main.handlebars

--- public/app.js
+++ public/app.js
@@ -1,54 +1,55 @@
 $(document).ready(function(){
 
     
-//display articles on screen 
-function displayArticles() {
-    $.getJSON("/articles", function(data) {
-        //loop through each article
-        for (var i = 0; i < data.length; i++) {
+    //display articles on screen 
+    function displayArticles() {
+        $.getJSON("/articles", function(data) {
+            //loop through each article
+            for (var i = 0; i < data.length; i++) {
 
-            //build each artcile
-            var newArticle = $("<div>");
-            newArticle.attr("class", "article");
-            newArticle.data("id", artilcle._id);
-            
+                //build each artcile
+                var newArticle = $("<div>");
+                newArticle.attr("class", "article");
+                newArticle.data("id", artilcle._id);
+                
 
-            var artHeadline = $("<h4>")
-            artHeadline.attr("class", "articleHeadline")
-            artHeadline.text(article.headline)
+                var artHeadline = $("<h4>")
+                artHeadline.attr("class", "articleHeadline")
+                artHeadline.text(article.headline)
 
-            var artSummary = $("<p>");
-            artSummary.attr("class", "articleSummary");
-            artSummary.text(article.summary);
+                var artSummary = $("<p>");
+                artSummary.attr("class", "articleSummary");
+                artSummary.text(article.summary);
 
-            var notesButton = $("</a>")
-            notesButton.attr("href", '/')
+                var notesButton = $("</a>")
+                notesButton.attr("href", '/')
 
-            
-            //add new articles
-            newArticle.append(artHeadline);
-            newArticle.append(artSummary)
-            $(".articles-cotainer").prepend(newArticle);
+                
+                //add new articles
+                newArticle.append(artHeadline);
+                newArticle.append(artSummary)
+                $(".articles-cotainer").prepend(newArticle);
 
-        }
-    })
-
-}
-
-//when page loads display articles
-displayArticles();
-
-
-$(document).on("click", ".scrapeButton", function() {
-    $.ajax({
-        method: "GET",
-        url: "/scrape"
-    })
-        .then(function(data) {
-            console.log(data);
-
-            displayArticles();
+            }
         })
-})
+
+    }
+
+    //when page loads display articles
+    displayArticles();
+
+
+    $('#scrapeButton').on("click", function() {
+        alert("button clicked")
+        $.ajax({
+            method: "GET",
+            url: "/scrape"
+        })
+            .then(function(data) {
+                console.log(data);
+
+                displayArticles();
+            })
+    })
 
 })
